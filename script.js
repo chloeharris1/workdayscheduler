336d8032bc12541eca162fb36f1a6718c27e7df4
Chloe Harris <chloe.a.harris17@gmail.com>
2021-09-30 15:23:49 -0700

    Updated HTML IDs and JS functions

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
 
-
 // Used Moment to display current date to header            
 var currentDay = moment().format("dddd, MMMM Do");
 $("#currentDay").text(currentDay);
 
 // GIVEN I am using a daily planner to create a schedule             
-// Create planner time block list                                  
+// Create planner time block list  
+                                
 
-
-
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// Add present, past, future CSS classes to hours list
+// Defining variables
 var saveBtn = $(".saveBtn");
 
 // each time block is color-coded to indicate whether it is in the past, present, or future
 function timeBlockColor() {
     var hour = moment().hours();
+    console.log(hour);
 
     $(".time-block").each(function () {
         var currHour = parseInt($(this).attr("id"));
-
+        console.log(currHour);
         // console.log(this); //each time-block
 
         if (currHour > hour) {
@@ -35,21 +30,9 @@ function timeBlockColor() {
     })
 };
 
-// WHEN I click the save button for that time block
-saveBtn.on("click", function () {
-
-    // console.log(this); //save button
-    var time = $(this).siblings(".hour").text();
-    var plan = $(this).siblings(".plan").val();
-
-    // THEN the text for that event is saved in local storage
-    localStorage.setItem(time, plan);
-});
-
 // WHEN I refresh the page
 // THEN the saved events persist
-function usePlanner() {
-
+function savePlanner() {
     $(".hour").each(function () {
         var currHour = $(this).text();
         var currPlan = localStorage.getItem(currHour);
@@ -63,18 +46,23 @@ function usePlanner() {
     });
 }
 
-/**
- * CALL FUNCTIONS
- */
+// // WHEN I click the save button for that time block
+// saveBtn.on("click", function () {
 
+//     console.log(this); //save button
+//     var time = $(this).siblings(".hour").text();
+//     var plan = $(this).siblings(".plan").val();
+
+//     // THEN the text for that event is saved in local storage
+//     localStorage.setItem(time, plan);
+// });
+
+saveBtn.on("click", savePlanner); 
+
+
+// // THEN the text for that event is saved in local storage
+// localStorage.setItem(time, plan);
+
+// Execute functions
 timeBlockColor();
-usePlanner();
-
-
-// WHEN I click into a timeblock
-// THEN I can enter an event
-    // Allow user to input information in each field
-
-// WHEN I refresh the page
-// THEN the saved events persist
-    // save session in browser
+savePlanner();
