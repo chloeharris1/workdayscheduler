28d5cd29f20d29dca24a74eca9a763220cb221a5
Chloe Harris <chloe.a.harris17@gmail.com>
2021-09-30 21:19:39 -0700

    Final versions of HTML, JS, CSS and README

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Used Moment to display current date to header            
 var currentDay = moment().format("dddd, MMMM Do");
 $("#currentDay").text(currentDay);
@@ -6,7 +5,6 @@ $("#currentDay").text(currentDay);
 // GIVEN I am using a daily planner to create a schedule             
 // Create planner time block list  
                                 
-
 // Defining variables
 var saveBtn = $(".saveBtn");
 
@@ -16,9 +14,9 @@ function timeBlockColor() {
     console.log(hour);
 
     $(".time-block").each(function () {
-        var currHour = parseInt($(this).attr("id"));
+        var currHour = parseInt($(this).attr("id"), 10);
+        
         console.log(currHour);
-        // console.log(this); //each time-block
 
         if (currHour > hour) {
             $(this).addClass("future");
@@ -34,34 +32,40 @@ function timeBlockColor() {
 // THEN the saved events persist
 function savePlanner() {
     $(".hour").each(function () {
-        var currHour = $(this).text();
-        var currPlan = localStorage.getItem(currHour);
+        // get the name of the div 
+        var name = String($(this).attr("name")).trim().toLowerCase()
 
-        // console.log(this);
-        // console.log(currHour);
+        // let's trim the name and check it in console
+        console.log("name " + name)
 
-        if (currPlan !== null) {
-            $(this).siblings(".plan").val(currPlan);
+        // retrieve the plan from storage 
+        var plan = localStorage.getItem(name)
+
+        // only display it in console/textbox if it's there, otherwise ditch the log spam
+        if (plan !== null) {
+            console.log("from storage: " + plan)
+            $(this).siblings(".textarea").val(plan)
         }
     });
 }
 
 // // WHEN I click the save button for that time block
-// saveBtn.on("click", function () {
+ saveBtn.on("click", function () {
+   // get the hour (same as the name)
+   var name = $(this).siblings(".hour").text().trim().toLowerCase()
 
-//     console.log(this); //save button
-//     var time = $(this).siblings(".hour").text();
-//     var plan = $(this).siblings(".plan").val();
+   // get the text entered in the box 
+   var text = $(this).siblings(".textarea").val()
 
-//     // THEN the text for that event is saved in local storage
-//     localStorage.setItem(time, plan);
-// });
+   // let's check the console for vars
+   console.log("name: " + name)
+   console.log("plan: " + text)
+
+   // THEN the text for that event is saved in local storage
+   localStorage.setItem(name, text);
+ });
 
 saveBtn.on("click", savePlanner); 
-
-
-// // THEN the text for that event is saved in local storage
-// localStorage.setItem(time, plan);
 
 // Execute functions
 timeBlockColor();
